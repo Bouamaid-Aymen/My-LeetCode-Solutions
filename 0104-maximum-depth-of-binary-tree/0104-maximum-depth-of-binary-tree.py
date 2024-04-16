5a8ed96750ae9498f540f38ea762af960d7e627c
Aymen Bouamaid <126708737+Bouamaid-Aymen@users.noreply.github.com>
2024-04-16 04:25:13 +0200

    Time: 38 ms (72.72%), Space: 17.7 MB (9.91%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -0,0 +1,15 @@
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+solution = Solution()
+print(solution.maxDepth(root))  
