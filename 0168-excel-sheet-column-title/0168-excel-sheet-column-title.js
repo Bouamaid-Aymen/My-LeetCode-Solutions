1ca74a85daac6a49848af899cc9c5426c291b209
Aymen Bouamaid <126708737+Bouamaid-Aymen@users.noreply.github.com>
2024-04-18 14:33:20 +0200

    Time: 57 ms (14.51%), Space: 48.8 MB (26.18%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+var convertToTitle = function(columnNumber) {
+    let result = '';
+
+    while (columnNumber > 0) {
+        let remainder = (columnNumber - 1) % 26;
+        result = String.fromCharCode(65 + remainder) + result;
+        columnNumber = Math.floor((columnNumber - 1) / 26);
+    }
+
+    return result;
+};
