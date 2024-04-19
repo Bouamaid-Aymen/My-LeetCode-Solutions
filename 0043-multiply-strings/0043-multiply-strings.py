7b4b83b53e33bcd6c805224bb88c98a5b50166e4
Aymen Bouamaid <126708737+Bouamaid-Aymen@users.noreply.github.com>
2024-04-19 19:46:00 +0100

    Time: 85 ms (22.93%), Space: 11.6 MB (95.17%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        if num1 == "0" or num2 == "0":
+            return "0"
+        
+        m, n = len(num1), len(num2)
+        result = [0] * (m + n)
+        
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                mul = (ord(num1[i]) - ord('0')) * (ord(num2[j]) - ord('0'))
+                p1, p2 = i + j, i + j + 1
+                summation = mul + result[p2]
+                result[p1] += summation // 10
+                result[p2] = summation % 10
+        
+        result_str = "".join(map(str, result)).lstrip('0')
+        return result_str if result_str else "0"
