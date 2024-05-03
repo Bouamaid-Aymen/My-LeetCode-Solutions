9aed494f0abc281c90293b54272da546884137da
Aymen Bouamaid <126708737+Bouamaid-Aymen@users.noreply.github.com>
2024-05-03 14:31:55 +0100

    Time: 55 ms (97.86%), Space: 56.7 MB (72.07%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -0,0 +1,25 @@
+var firstMissingPositive = function(nums) {
+    const n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] <= 0 || nums[i] > n) {
+            nums[i] = n + 1;
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        const num = Math.abs(nums[i]);
+        if (num > 0 && num <= n) {
+            nums[num - 1] = -Math.abs(nums[num - 1]);
+        }
+    }
+
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] > 0) {
+            return i + 1;
+        }
+    }
+
+    return n + 1;
+};
