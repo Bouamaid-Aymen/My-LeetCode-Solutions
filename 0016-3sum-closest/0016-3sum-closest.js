46abdce09854014dbd7078c6cb19842e278e8d31
Aymen Bouamaid <126708737+Bouamaid-Aymen@users.noreply.github.com>
2024-05-05 12:20:43 +0100

    Time: 59 ms (95.88%), Space: 50.7 MB (70.91%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -0,0 +1,33 @@
+var threeSumClosest = function(nums, target) {
+    nums.sort((a, b) => a - b); 
+    let closestSum = Infinity;
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (sum === target) {
+                return sum; 
+            }
+
+        
+            if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                closestSum = sum;
+            }
+
+            if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return closestSum;
+};
+
+console.log(threeSumClosest([-1, 2, 1, -4], 1));
+console.log(threeSumClosest([0, 0, 0], 1)); 
