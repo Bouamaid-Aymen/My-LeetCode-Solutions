eb16860606b1348736748ea168119bf16c12d4b0
Aymen Bouamaid <126708737+Bouamaid-Aymen@users.noreply.github.com>
2024-05-08 19:58:57 +0100

    Time: 71 ms (5.52%), Space: 51.4 MB (16.88%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.js
@@ -0,0 +1,23 @@
+var binaryTreePaths = function(root) {
+    let paths = [];
+
+    const dfs = (node, path) => {
+        if (!node) {
+            return;
+        }
+
+        path += node.val.toString();
+
+        if (!node.left && !node.right) {
+            paths.push(path);
+        } else {
+            path += '->';
+            dfs(node.left, path);
+            dfs(node.right, path);
+        }
+    };
+
+    dfs(root, '');
+
+    return paths;
+};
